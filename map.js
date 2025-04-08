ada1fe12f4e196c893b4b7aa9c7c4e61bd9af48a
Renee Pearlin <reneepearlin@gmail.com>
2025-04-08 19:07:06 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,44 +1,30 @@
-// map.js
-import { db, ref, onValue } from './firebase.js';
+let map;
+const markers = {};
 
-const map = L.map('map').setView([0, 0], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
-
-const fleetMap = L.map('fleet-map').setView([0, 0], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(fleetMap);
-
-const gpsRef = ref(db, 'gpsData');
-let marker;
-
-onValue(gpsRef, (snap) => {
-  const data = snap.val();
-  if (!data) return;
-  const { latitude, longitude } = data;
-  if (marker) {
-    marker.setLatLng([latitude, longitude]);
-  } else {
-    marker = L.marker([latitude, longitude]).addTo(map);
-  }
-  map.setView([latitude, longitude], 15);
-});
-
-const fleetRef = ref(db, 'fleet');
-let fleetMarkers = {};
-
-onValue(fleetRef, (snap) => {
-  snap.forEach(bus => {
-    const data = bus.val();
-    const id = bus.key;
-    if (fleetMarkers[id]) {
-      fleetMarkers[id].setLatLng([data.latitude, data.longitude]);
-    } else {
-      fleetMarkers[id] = L.marker([data.latitude, data.longitude])
-        .addTo(fleetMap)
-        .bindPopup(`Bus ${id}`);
-    }
+window.initMap = function () {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 12.9716, lng: 77.5946 },
+    zoom: 12,
   });
+};
+
+function addOrUpdateMarker(busId, lat, lng) {
+  if (markers[busId]) {
+    markers[busId].setPosition({ lat, lng });
+  } else {
+    markers[busId] = new google.maps.Marker({
+      position: { lat, lng },
+      map,
+      label: busId,
+    });
+  }
+}
+
+window.addEventListener("updateBus", (e) => {
+  const { busId, lat, lng } = e.detail;
+  addOrUpdateMarker(busId, lat, lng);
 });
+
+window.onload = () => {
+  initMap();
+};
