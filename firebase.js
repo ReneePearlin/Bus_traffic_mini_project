1a42a1de7a68d688cce3a5c04ffe946583098b1a
Renee Pearlin <reneepearlin@gmail.com>
2025-04-08 20:00:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,47 +1,30 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
+  getAuth, GoogleAuthProvider, signInWithPopup,
+  signInWithEmailAndPassword, createUserWithEmailAndPassword,
+  signOut, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 import {
-  getDatabase,
-  ref,
-  onValue
+  getDatabase, ref, onValue
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// ✅ Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyATTUPboq4w0nwxubk5e5tL9ujOiSx03aU",
-  authDomain: "trackmyride-5524a.firebaseapp.com",
-  projectId: "trackmyride-5524a",
-  storageBucket: "trackmyride-5524a.appspot.com",
-  messagingSenderId: "275723082314",
-  appId: "1:275723082314:web:54b9c80fecba4e414b24b3",
-  databaseURL: "https://trackmyride-5524a-default-rtdb.firebaseio.com"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  databaseURL: "YOUR_DATABASE_URL"
 };
 
-// ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 const provider = new GoogleAuthProvider();
 
 export {
-  auth,
-  provider,
-  db,
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  ref,
-  onValue
+  auth, provider, db, signInWithPopup, signInWithEmailAndPassword,
+  createUserWithEmailAndPassword, signOut, onAuthStateChanged, ref, onValue
 };
