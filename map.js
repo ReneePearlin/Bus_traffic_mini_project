55553a0b1c2c165868fd31bfc4ba92a6b9931b72
Renee Pearlin <reneepearlin@gmail.com>
2025-04-08 20:22:10 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,20 +1,32 @@
 let map;
-window.renderBusesOnMap = (busData) => {
-  for (let busId in busData) {
-    const { lat, lng } = busData[busId];
-    new google.maps.Marker({
-      position: { lat, lng },
-      map,
-      title: `Bus ${busId}`
-    });
-  }
+const busMarkers = {};
+
+window.initMap = function () {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: { lat: 12.9716, lng: 77.5946 } // default to Bangalore
+  });
 };
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 0, lng: 0 },
-    zoom: 2
-  });
+function addOrUpdateMarker(busId, position) {
+  if (busMarkers[busId]) {
+    busMarkers[busId].setPosition(position);
+  } else {
+    const marker = new google.maps.Marker({
+      position,
+      map,
+      label: busId,
+      title: `Bus ${busId}`
+    });
+    busMarkers[busId] = marker;
+  }
 }
-window.initMap = initMap;
-window.onload = initMap;
+
+window.addEventListener('busLocationUpdate', e => {
+  const { busId, position } = e.detail;
+  addOrUpdateMarker(busId, position);
+});
+
+window.onload = () => {
+  if (typeof google !== 'undefined') initMap();
+};
