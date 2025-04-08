29d95eac7fb82a15066f9d714539718745055f9e
Renee Pearlin <reneepearlin@gmail.com>
2025-04-08 20:19:30 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
-  getAuth, GoogleAuthProvider, signInWithPopup,
-  signInWithEmailAndPassword, createUserWithEmailAndPassword,
-  signOut, onAuthStateChanged
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 import {
-  getDatabase, ref, onValue
+  getDatabase,
+  ref,
+  onValue
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
+// Replace with your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyATTUPboq4w0nwxubk5e5tL9ujOiSx03aU",
   authDomain: "trackmyride-5524a.firebaseapp.com",
@@ -18,13 +25,20 @@ const firebaseConfig = {
   appId: "1:275723082314:web:54b9c80fecba4e414b24b3",
   measurementId: "G-9P4N8QVMTE"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 const provider = new GoogleAuthProvider();
 
 export {
-  auth, provider, db, signInWithPopup, signInWithEmailAndPassword,
-  createUserWithEmailAndPassword, signOut, onAuthStateChanged, ref, onValue
+  auth,
+  provider,
+  db,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  ref,
+  onValue
 };
