6fbf3694e96afcafe5bad795fd41475ab97f1ee1
Renee Pearlin <reneepearlin@gmail.com>
2025-04-08 20:03:33 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,30 +1,20 @@
 let map;
-const markers = {};
-
-window.initMap = function () {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 12.9716, lng: 77.5946 },
-    zoom: 12,
-  });
-};
-
-function addOrUpdateMarker(busId, lat, lng) {
-  if (markers[busId]) {
-    markers[busId].setPosition({ lat, lng });
-  } else {
-    markers[busId] = new google.maps.Marker({
+window.renderBusesOnMap = (busData) => {
+  for (let busId in busData) {
+    const { lat, lng } = busData[busId];
+    new google.maps.Marker({
       position: { lat, lng },
       map,
-      label: busId,
+      title: `Bus ${busId}`
     });
   }
-}
-
-window.addEventListener("updateBus", (e) => {
-  const { busId, lat, lng } = e.detail;
-  addOrUpdateMarker(busId, lat, lng);
-});
-
-window.onload = () => {
-  initMap();
 };
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 0, lng: 0 },
+    zoom: 2
+  });
+}
+window.initMap = initMap;
+window.onload = initMap;
